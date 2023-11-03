20f619e79e2f73c8df7c0a1a813ee33dba4ccbde
dennischou <dses5900@gmail.com>
2023-11-03 11:08:04 +0800

    Add hpa memory test

--- src/main/java/com/rh/GreetingResource.java
+++ src/main/java/com/rh/GreetingResource.java
@@ -1,7 +1,12 @@
 package com.rh;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
@@ -12,5 +17,18 @@ public class GreetingResource {
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
         return "Hello from RESTEasy Reactive";
+    }
+
+    @GET
+    @Path("fill/{index}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String fill(@PathParam("index") String index) {
+        Map<String, String> mem = new HashMap<>();
+        char[] chars = new char[2 * 1024 * 1024];
+        String log = "Added " + index + "MB";
+        Arrays.fill(chars, 'f');
+        mem.put(Math.random() + "", new String(chars));
+        System.out.println(log);
+        return log + "\n";
     }
 }
